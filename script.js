6c1b40838b84cac68d2b1f40c0a996059aeabd0e
Bamboo Forest <113477194+Mr-Bamboo-Forest@users.noreply.github.com>
2024-04-05 12:46:32 +1000

    start glitch fixed + missing semicolons fixed

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
-const startEl = document.getElementById("start")
-const stopEl = document.getElementById("stop")
-const resetEl = document.getElementById("reset")
-const timerEl = document.getElementById("timer")
+const startEl = document.getElementById("start");
+const stopEl = document.getElementById("stop");
+const resetEl = document.getElementById("reset");
+const timerEl = document.getElementById("timer");
 
-let interval
+let interval;
 let timeLeft = 1500;
+let timerRunning = false;
 
-function updateTimer(){
+function updateTimer() {
     let minutes = Math.floor(timeLeft / 60);
     let seconds = timeLeft % 60;
-    let formattedTime = `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2, "0")}`;
+    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 
     timerEl.innerHTML = formattedTime;
 }
 
-function startTimer(){
-    interval = setInterval(()=>{
-        timeLeft--;
-        updateTimer();
-        if(timeLeft === 0){
-            clearInterval(interval)
-            alert("Time's Up!")
-            timeLeft = 1500;
+function startTimer() {
+    if (!timerRunning) {
+        timerRunning = true;
+        interval = setInterval(() => {
+            timeLeft--;
             updateTimer();
-        }
-    }, 1000)
+            if (timeLeft === 0) {
+                clearInterval(interval);
+                alert("Time's Up!");
+                timeLeft = 1500;
+                updateTimer();
+                timerRunning = false;
+            }
+        }, 1000);
+    }
 }
-function stopTimer(){
+
+function stopTimer() {
     clearInterval(interval);
+    timerRunning = false;
 }
-function resetTimer(){
+
+function resetTimer() {
     clearInterval(interval);
     timeLeft = 1500;
     updateTimer();
+    timerRunning = false;
 }
 
-startEl.addEventListener("click", startTimer)
-stopEl.addEventListener("click", stopTimer)
-resetEl.addEventListener("click", resetTimer)
+startEl.addEventListener("click", startTimer);
+stopEl.addEventListener("click", stopTimer);
+resetEl.addEventListener("click", resetTimer);
